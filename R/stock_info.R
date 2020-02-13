b763340bfaf465ee6e55351c10b0d170576dd1f0
rijinbaby <rijinbaby26@gmail.com>
2020-02-13 15:20:23 +0100

    final update

--- R/stock_info.R
+++ R/stock_info.R
@@ -1,14 +1,14 @@
 
 # stock_details() function -----------------------------------------------------------
 
-#' Function that displays and plot the stock data
+#' Function that displays and plot the stock data. By default the function gives 10 years data for the stock if the start and end dates are not specified
 #' @param stock_name a sting that represent the symbol of the Company listed in NASDAQ
 #' @param start_date a sting that represent the date; By default 10 years from current date is considered
 #' @param end_date a sting that represent the date; Current date is taken as default if no value is provided
 #' @return Data and plot
 #' @author Rijin Baby
 #' @details
-#' The function initially cleans the entered argumets and check their validity. Then collect the data file,
+#' The function initially cleans the entered argumets and check their validity. Then update the data file and finally
 #' dispalys the data for the analysis
 #' @export
 #' @seealso \code{plot_ly}\code{layout}
@@ -28,14 +28,14 @@ stock_details <- function(stock_name, start_date=Sys.Date()-years(10),end_date=S
   # Calling the function to check the arguments
   argument_validation(stock_name,start_date,end_date)
 
-  # Calling check_file() function for the file path
-  dest <- check_file(stock_name)
+  # Calling get_data() function for the file path
+  file_location <- get_data(stock_name)
 
   # reading the data
-  Stock_Info <- read.csv(dest, stringsAsFactors=FALSE)
+  Stock_Info <- read.csv(file_location, stringsAsFactors=FALSE)
 
-  n <- which(colnames(Stock_Info)=="Close.Last")
-  colnames(Stock_Info)[n] <- "Close_Price"
+  column_index <- which(colnames(Stock_Info)=="Close.Last")
+  colnames(Stock_Info)[column_index] <- "Close_Price"
 
   # Removing the $ sign from the data columns
   Stock_Info[,c("Close_Price","Open","High","Low")] <- as.data.frame(apply
@@ -61,40 +61,39 @@ stock_details <- function(stock_name, start_date=Sys.Date()-years(10),end_date=S
   plotly::plot_ly(Stock_Info, x = ~Date, y = ~Close_Price, type = 'scatter', mode = 'lines',color = I(trend_color)) %>%
   plotly::layout(title =paste0(nasdaq::nasdaq_listed$Security_Name[which(stock_name==nasdaq::nasdaq_listed$Symbol)]," (",stock_name,")"))
 
-  }
+}
 
-# check_file() function --------------------------------------------------------------
+# get_data() function --------------------------------------------------------------
 
 #Check if the data already exist in the directory else download and return the file path
-#' Function that search for data file
-#' @param stock_name a sting that represent the symbol of the Company listed in NASDAQ
-#' @return file path of the csv file
+#' Function that search and download the data file
+#' @param stock_name a string that represent the symbol of the Company listed in NASDAQ
+#' @return temporary file path of the csv file
 #' @author Rijin Baby
 #' @details
-#' This function initially checks if the file with stock details is present in the directory or not.
-#' If the file is found in the directory the file path is returned else this function download the file
-#' from the NASDAQ website and return the file path. In case of no data present in the file the function terminates the process
+#' This function download the stock data fro the NASDAQ website to a temporary directory in the system
+#' and return the file path for better memory management.
 #' @export
 #' @seealso \code{download.file}
 #' @importFrom utils download.file
 #' @importFrom lubridate years
 
-check_file <- function(stock_name)
+get_data <- function(stock_name)
 {
   # Construct web URL
-  src <- paste0("https://www.nasdaq.com/api/v1/historical/",stock_name,"/stocks/",Sys.Date()-years(10),"/",Sys.Date())
+  src_website <- paste0("https://www.nasdaq.com/api/v1/historical/",stock_name,"/stocks/",Sys.Date()-years(10),"/",Sys.Date())
 
   # Construct path for storing local file temporarily
-  dest <- tempfile(stock_name,fileext = ".csv")
+  file_location <- tempfile(stock_name,fileext = ".csv")
 
   # Don't download if the file is already there!
-  if(!file.exists(dest))
-    download.file(src, dest, quiet = TRUE)
+  if(!file.exists(file_location))
+    download.file(src_website, file_location, quiet = TRUE)
 
   # Checking if the file has data or not
-  my_text<-readLines(dest)
-  ifelse(my_text==""&length(my_text)==1,stop("NO DATA FOUND ON THE WEBSITE, KINDLY MODIFY THE INPUT DATE"),
-         return(dest))
+  file_check<-readLines(file_location)
+  ifelse(file_check==""&length(file_check)==1,stop("NO DATA FOUND ON THE WEBSITE, KINDLY MODIFY THE INPUT DATE"),
+         return(file_location))
 }
 
 # argument_validation() function -----------------------------------------------------
